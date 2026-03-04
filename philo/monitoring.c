7fa22d170bb1ce09431d014a69c6ec29617a5188
Aleyna Say <sayaleyna27@gmail.com>
2026-03-04 15:42:28 +0300

    Update monitoring to end simulation when all philos reach meal limit.

--- philo/monitoring.c
+++ philo/monitoring.c
@@ -12,23 +12,44 @@
 
 #include "philo.h"
 
-int death_ctrl(t_main *main, t_philo *ptr)
+int death_ctrl(t_main *main)
 {
+    int i;
     long past_time;
 
-    pthread_mutex_lock(&main->meal_mutex);
-    past_time = convert_time() - ptr->last_meal;
-    pthread_mutex_unlock(&main->meal_mutex);
-    if(past_time >= main->die_time) 
+    i = 0;
+    while(i < main->philo_num)
+    {
+        pthread_mutex_lock(&main->meal_mutex);
+        past_time = convert_time() - main->philos[i].last_meal;
+        pthread_mutex_unlock(&main->meal_mutex);
+        if(past_time >= main->die_time)
+        {
+            pthread_mutex_lock(&main->dead_mutex);
+            if(main->rudead == 1)
+            {
+                pthread_mutex_unlock(&main->dead_mutex);
+                return 1;
+            }
+            main->rudead = 1;
+            pthread_mutex_unlock(&main->dead_mutex);
+            pthread_mutex_lock(&main->write_mutex);
+            printf("%ld\t%d died.\n", elapsed_time(main), main->philos[i].philo_id);
+            pthread_mutex_unlock(&main->write_mutex);
             return 1;
+        }
+        i++;
+    }
     return 0;
 }
 
 int eat_count_ctrl(t_main *main)
 {
     int i;
+    int full_philos;
 
     i = 0;
+    full_philos = 0;
     pthread_mutex_lock(&main->meal_mutex);
     if(main->must_eat <= 0)
     {
@@ -36,50 +57,33 @@ int eat_count_ctrl(t_main *main)
         return 0;
     }
     pthread_mutex_unlock(&main->meal_mutex);
-    while (i < main->philo_num)
+    while(i < main->philo_num)
     {
-        if(main->philos[i].eat_num > main->must_eat)
-        {
-            pthread_mutex_unlock(&main->meal_mutex);
-            return 0;
-        }
+        pthread_mutex_lock(&main->meal_mutex);
+        if(main->philos[i].eat_num >= main->must_eat)
+            full_philos++;
+        pthread_mutex_unlock(&main->meal_mutex);
         i++;
-    } 
-    pthread_mutex_unlock(&main->meal_mutex);
-    return 1;
+    }
+    if(full_philos == main->philo_num)
+        return 1;
+    return 0;
 }
 
-void *monitor_routine(void *arg)
+void monitoring(t_main *main)
 {
-    t_main *main;
-    int i;
-    
-    main = (t_main *)arg;
-    while(1)
+    while (1)
     {
-        i = 0;  
-        while(i < main->philo_num)
+        if (death_ctrl(main) == 1)
+            return ;
+        
+        if (eat_count_ctrl(main) == 1)
         {
-            if(death_ctrl(main, &main->philos[i]))
-            {
-                pthread_mutex_lock(&main->dead_mutex);
-                main->rudead = 1;
-                pthread_mutex_unlock(&main->dead_mutex);
-                pthread_mutex_lock(&main->write_mutex);
-                printf("%ld\t%d died\n", convert_time() - main->start, main->philos[i].philo_id);
-                pthread_mutex_unlock(&main->write_mutex);
-                return NULL;
-            }
-            if(eat_count_ctrl(main))
-            {
-                pthread_mutex_lock(&main->dead_mutex);
-                main->rudead = 1;
-                pthread_mutex_unlock(&main->dead_mutex);                
-                return NULL;
-            }
-            i++;
+            pthread_mutex_lock(&main->dead_mutex);
+            main->rudead = 1;
+            pthread_mutex_unlock(&main->dead_mutex);
+            return;
         }
-        usleep(500);
+        usleep(1000);
     }
-    return NULL;
 }
